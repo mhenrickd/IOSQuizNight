d7f120746ec61e31de62a5923c33587ec14afb71
Matheus Henrick Dias <matheushd@ciandt.com>
2020-03-01 15:42:12 -0300

    creating layout using the autoLayout

--- quizNight/ViewController.swift
+++ quizNight/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var quizNightLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
